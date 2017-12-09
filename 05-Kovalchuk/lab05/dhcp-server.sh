53777a047cc44573ead7b1edc0edc6d380210c92
Oleksandr Kovalchuk <anxolerd@outlook.com>
2017-12-09 15:58:44 +0200

    Finalize dhcpd lab (05)

--- 05-Kovalchuk/lab05/dhcp-server.sh
+++ 05-Kovalchuk/lab05/dhcp-server.sh
@@ -18,18 +18,38 @@ option domain-name-servers 10.18.49.102;
 default-lease-time 300;
 max-lease-time 300;
 
-# If this DHCP server is the official DHCP server for the local
-# network, the authoritative directive should be uncommented.
-#authoritative;
-
 subnet 10.18.51.0 netmask 255.255.255.0 {
   range 10.18.51.90 10.18.51.94;
   range 10.18.51.98 10.18.51.98;
   option routers 10.18.51.1;
 }
 
-host starfire {
-  hardware ethernet 08:00:07:26:c0:a5;
+host polar.anxolerd.net {
   fixed-address 10.18.51.99;
 }
+EOF
+
+# Create empty leases database
+touch /var/db/dhcpd.leases
+
+# ========
+# Test run
+# ========
+
+ifconfig em1 10.18.51.100/24
+service isc-dhcpd onestart
+
+# =====================
+# Run server on startup
+# =====================
+cat >/etc/rc.conf <<EOF
+#!/bin/sh
+ifconfig_em0="DHCP"
+
+# The following two are an important lines
+ifconfig_em1="inet 10.18.51.100 netmask 255.255.255.0"
+dhcpd_enable="YES"
+
+sshd_enable="YES"
+hostname="monica"
 EOF
